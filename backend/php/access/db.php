15649868a0b61a8e358338cb8f3375eeae01ddf3
Metz01 <camillomatteo01@gmail.com>
2024-01-13 01:55:41 +0100

    Update login page styles and fix JavaScript validation

---
+++ backend/php/access/db.php
@@ -0,0 +1,80 @@
+
+<?php
+
+class DataBase
+{
+    private static $connection;
+
+    public function connect($hostname = "localhost", $username = "toni", $password = "admin", $database = "toni")
+    {
+        try {
+            if (!isset(self::$connection)) {
+                self::$connection = new mysqli($hostname, $username, $password, $database);
+            }
+            // Check the connection
+            if (self::$connection->connect_error) {
+                die("Connection failed: " . self::connection->connect_error);
+            }
+        } catch (Exception $e) {
+            server_error();
+        }
+    }
+
+    public function runQuery($query, ...$params)
+    {
+        // Check if the connection has been established
+        if (!self::$conn) {
+            error_log("Error: Connection not established. Conencting with default parameters.");
+            $this->connect();
+        }
+
+        // Prepare the statement
+        $stmt = self::$conn->prepare($query);
+
+        // Check if the statement was prepared successfully
+        if (!$stmt) {
+            return "Error: " . self::$conn->error;
+        }
+
+        // Dynamically bind parameters
+        if (!empty($params)) {
+            $types = str_repeat('s', count($params));
+            $stmt->bind_param($types, ...$params);
+        }
+
+        // Execute the query
+        $result = $stmt->execute();
+
+        // Check if the query was successful
+        if ($result) {
+            $data = [];
+
+            // Get results
+            $result = $stmt->get_result();
+
+            // Fetch the results
+            while ($row = $result->fetch_assoc()) {
+                $data[] = $row;
+            }
+
+            // Free the result set
+            $result->free();
+        } else {
+            // Return an error message if the query fails
+            $data = "Error: " . $query . "<br>" . $stmt->error;
+        }
+
+        // Close the statement
+        $stmt->close();
+
+        return $data;
+    }
+
+    public function closeConnection()
+    {
+        // Close the database connection
+        $this->conn->close();
+    }
+}
+
+?>
