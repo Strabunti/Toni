8c4343aa802f5c414eccdb22b6419976af51ed85
filippotonietto <filippo.tonietto@gmail.com>
2024-01-30 23:21:23 +0100

    Finally working.

--- scripts/popUp.js
+++ scripts/popUp.js
@@ -1,8 +1,8 @@
 function openPopup(name, description, ingredients, price, imageSrc) {
 
-    document.getElementById('popup-name').innerText = name.replace(/'/g, "\\'");
-    document.getElementById('popup-description').innerText = description.replace(/'/g, "\\'");
-    document.getElementById('popup-ingredients').innerText = ingredients.replace(/'/g, "\\'");
+    document.getElementById('popup-name').innerText = name;
+    document.getElementById('popup-description').innerText = description;
+    document.getElementById('popup-ingredients').innerText = ingredients;
     document.getElementById('popup-price').innerText = 'Prezzo: ' + price;
     document.getElementById('popup-image2').src = imageSrc;
     document.getElementById('popup').style.display = 'inline-block';
